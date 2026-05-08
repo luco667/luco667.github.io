ee83d3a8e979cd4d96e1b6af39ce22317f4991b5
luco667 <72415108+luco667@users.noreply.github.com>
2026-05-08 06:09:50 +0200

    Update script.js

--- snakepages/script.js
+++ snakepages/script.js
@@ -105,6 +105,38 @@ function getPreviousLines() {
   text += currentLine;
   return text;
 }
+/* =========================================
+   ACTIVE NAV LINK ON SCROLL
+========================================= */
 
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 140;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === `#${current}`) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
 // Start typewriter after short delay
 setTimeout(type, 600);
