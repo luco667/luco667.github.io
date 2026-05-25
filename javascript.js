b14fd0a46de79c67303e239c31e5ac2ab9a7f633
luco667 <72415108+luco667@users.noreply.github.com>
2026-05-25 18:37:54 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -383,8 +383,8 @@ const PROJECTS = [
     .projects-scroll::-webkit-scrollbar { display: none; }
 
     .cat-card {
-      min-width: 210px;
-      max-width: 210px;
+      min-width: 250px;
+      max-width: 250px;
       border: 1px solid rgba(0,255,65,0.2);
       background: rgba(0,10,0,0.75);
       padding: 20px;
