01c77afe1f6cadebd5d0b9061e20ec1e4fefcd92
luco667 <72415108+luco667@users.noreply.github.com>
2026-06-16 16:37:19 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -508,6 +508,21 @@ if (!output || !term) {
 console.log("term", term.scrollHeight, term.clientHeight);
 console.log("body", document.body.scrollHeight);
 console.log("term", term.scrollHeight, term.clientHeight);
+console.log("term", term.scrollHeight, term.clientHeight);
+
+let el = term;
+
+while (el) {
+  console.log(
+    el.tagName,
+    el.className,
+    getComputedStyle(el).overflowY,
+    el.scrollHeight,
+    el.clientHeight
+  );
+  el = el.parentElement;
+}
+
 
 let el = term;
 
