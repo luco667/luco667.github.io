a928ff668029a90d113c71365c5a9c305638e01a
luco667 <72415108+luco667@users.noreply.github.com>
2026-07-14 14:14:41 +0200

    Update projects.js

--- projects.js
+++ projects.js
@@ -121,7 +121,7 @@ const PROJECTS = [
         name: "Curriculum Vitae",
         desc: "",
         tags: [CV],
-        link: "Projects/Miscellaneous/Curriculum_Vitae/CV.html",
+        link: "Projects/Curriculum_Vitae/CV.html",
         status: "done",
         date: "2026"
       }
